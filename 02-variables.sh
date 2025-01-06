d3b4ba565ffad915cad74ebc9fa5d86bb037d7ff
Raghu <raghu.mobility@gmail.com>
2025-01-06 15:21:35 +0530

    script added

--- 02-variables.sh
+++ 02-variables.sh
@@ -4,6 +4,6 @@ PERSON1=Ramesh
 PERSON2=Suresh
 
 echo "$PERSON1 :: Hi $PEROSN2"
-echo "$PEROSN2 :: Hello $PERSON1"
+echo "$PERSON2 :: Hello $PERSON1"
 echo "$PERSON1 :: How are you doing"
 echo "$PERSON2 :: I am good, how are you"
