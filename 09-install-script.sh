56381026021252d914e04c760e79b1802fa634df
Raghu <raghu.mobility@gmail.com>
2025-01-06 21:27:00 +0530

    script added

--- 09-install-script.sh
+++ 09-install-script.sh
@@ -5,6 +5,24 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
    echo "Error: You must have sudo access"
+   exit 1
 fi
 
 dnf install mysql -y 
+
+if [ $? -ne 0]
+then
+echo "Installing MySql ... Failure"
+exit 1
+else
+echo " Installing MySql ...Success
+
+
+dnf install git -y 
+
+if [ $? -ne 0]
+then
+echo "Installing Git ... Failure"
+exit 1
+else
+echo " Installing Git ...Success
